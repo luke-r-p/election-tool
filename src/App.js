f081641f1712cebd163973a31932d57879860162
luke-r-p <l.philipson72@gmail.com>
2026-09-16 17:41:26 +0100

    Expanded input settings implementation, onChange currently not working

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import { useState } from 'react';
 import './App.css';
-import InputSettings from './InputSettings'
+import {InputSettings, InputSettingsValues} from './InputSettings'
 
 // main function
 function App() {
+  // contains data for the input settings
+  const [inputSettingsValues, setInputSettingsValues] = useState(new InputSettingsValues(1, 0, 0));
+
   return (
     <div className="App">
       <div className='groupInput'>
         <h3>Settings:</h3>
-        <InputSettings/>
+        <InputSettings setter={setInputSettingsValues}/>
         <h3>Parties:</h3>
+        <p>{inputSettingsValues.seats}</p>
+        <p>{inputSettingsValues.minVotes}</p>
+        <p>{inputSettingsValues.allocation}</p>
       </div>
       <div id='diagramDiv'></div>
     </div>
--- src/InputSettings.js
+++ src/InputSettings.js
@@ -1,17 +1,44 @@
 import './App.css';
 
-export default function InputSettings() {
+export function InputSettings(setter) {
+  // updates the settings values
+  function updateInputSettings() {
+    console.log("Called");
+    
+    try {
+      // gets the values from the inputs
+      let s = parseInt(document.getElementById('totalSeats').value);
+      let m = parseInt(document.getElementById('minVotes').value);
+      let a = document.getElementById('allocation').value;
+
+      // updates state
+      setter(new InputSettingsValues(s, m, a));
+
+      console.log("Updated");
+    } catch {}
+  }
+
+  // returns the html inputs
   return (
     <div>
       <label htmlFor='totalSeats'>Seats: </label>
-      <input type='number' name='totalSeats' className='inputSettingsInput'/>
+      <input type='number' id='totalSeats' name='totalSeats' className='inputSettingsInput' min='1' defaultValue='1' onInput={updateInputSettings()}/>
       <label htmlFor='minVotes'>Min. votes: </label>
-      <input type='number' name='minVotes' className='inputSettingsInput'/>
+      <input type='number' id='minVotes' name='minVotes' className='inputSettingsInput' min='0' defaultValue='0' onInput={updateInputSettings()}/>
       <label htmlFor='allocation'>Allocation method: </label>
-      <select name='allocation'>
-        <option value='0'>D'Hondt</option>
-        <option value='1'>Sainte-Laguë</option>
+      <select id='allocation' name='allocation' onChange={updateInputSettings()}>
+        <option value={0}>D'Hondt</option>
+        <option value={1}>Sainte-Laguë</option>
       </select>
     </div>
   )
+}
+
+// class to hold the settings values
+export class InputSettingsValues {
+  constructor(seats, minVotes, allocation) {
+    this.seats = seats;
+    this.minVotes = minVotes;
+    this.allocation = allocation;
+  }
 }
