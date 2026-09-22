20971c9ad8976544c31c747d9967d8e0a0909baa
luke-r-p <l.philipson72@gmail.com>
2026-09-22 15:32:21 +0100

    Created party data and connected it to input elements, no state update implemented yet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import {InputSettings, InputSettingsValues, updateInputSettings} from './InputSettings'
-import {PartyTable} from './PartyTable'
+import {PartyTable, Party} from './PartyTable'
 
 // main function
 function App() {
@@ -16,13 +16,16 @@ function App() {
     }
   }
 
+  // contains the data for all of the parties
+  const [partyData, setPartyData] = useState([new Party(0, 0, 0, 0), new Party(0, 0, 0, 0), new Party(0, 0, 0, 0)]);
+
   return (
     <div className="App">
       <div className='groupInput'>
         <h3>Settings:</h3>
         <InputSettings handler={handleInputSettings}/>
         <h3>Parties:</h3>
-        <PartyTable/>
+        <PartyTable data={partyData}/>
       </div>
       <div id='diagramDiv'></div>
     </div>
--- src/PartyTable.js
+++ src/PartyTable.js
@@ -1,7 +1,7 @@
 import './App.css';
 import './PartyTable.css';
 
-export function PartyTable() {
+export function PartyTable({data}) {
   return (
     <table>
       <tr>
@@ -11,32 +11,41 @@ export function PartyTable() {
         <th>Final Seats</th>
       </tr>
       <tr>
-        <td><input type='color'/></td>
-        <td><input type='number'/></td>
-        <td><input type='number'/></td>
-        <td>Seats</td>
+        <td><input type='color' value={data[0].colour}/></td>
+        <td><input type='number' value={data[0].votes}/></td>
+        <td><input type='number' value={data[0].inseats}/></td>
+        <td>{data[0].fseats}</td>
         <button>X</button>
         <button>🡑</button>
         <button>🡓</button>
       </tr>
       <tr>
-        <td><input type='color'/></td>
-        <td><input type='number'/></td>
-        <td><input type='number'/></td>
-        <td>Seats</td>
+        <td><input type='color' value={data[1].colour}/></td>
+        <td><input type='number' value={data[1].votes}/></td>
+        <td><input type='number' value={data[1].inseats}/></td>
+        <td>{data[1].fseats}</td>
         <button>X</button>
         <button>🡑</button>
         <button>🡓</button>
       </tr>
       <tr>
-        <td><input type='color'/></td>
-        <td><input type='number'/></td>
-        <td><input type='number'/></td>
-        <td>Seats</td>
+        <td><input type='color' value={data[2].colour}/></td>
+        <td><input type='number' value={data[2].votes}/></td>
+        <td><input type='number' value={data[2].inseats}/></td>
+        <td>{data[2].fseats}</td>
         <button>X</button>
         <button>🡑</button>
         <button>🡓</button>
       </tr>
     </table>
   )
+}
+
+export class Party {
+  constructor(colour, votes, inseats, fseats) {
+    this.colour = colour;
+    this.votes = votes;
+    this.inseats = inseats;
+    this.fseats = fseats;
+  }
 }
